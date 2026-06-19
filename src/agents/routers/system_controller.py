fa7a89ae7940d1b4db4849363b9f682df5cf3800
Turkov Leonid <100690100+LeonDeTur@users.noreply.github.com>
2026-06-19 18:28:34 +0300

    v0.10.2
    * feat(system): (#72)
    
    - added app configuration getter
    - upgraded version to 0.10.2

--- src/agents/routers/system_controller.py
+++ src/agents/routers/system_controller.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
 
 from src.agents.dependencies.dependencies import get_system_service
+from src.agents.schema.app_config_response import AppConfigResponse
 from src.agents.services.system_service import SystemService
 
 system_router = APIRouter(prefix="/system", tags=["system"])
@@ -21,3 +22,14 @@ async def get_system_logs(
         path=system_service.log_path,
         filename=f"idu-agents-{datetime.now().strftime('%Y%m%d-%H%M%S')}.log",
     )
+
+
+@system_router.get("/config")
+async def get_app_config(
+    system_service: SystemService = Depends(get_system_service),
+) -> AppConfigResponse:
+    """
+    Get the current agents service runtime configuration.
+    """
+
+    return system_service.get_app_config()
